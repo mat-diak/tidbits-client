3f6520fb275baaa5ab07099c17b2e6f4ec17dac0
Josh <joshlim092@gmail.com>
2022-03-02 16:40:39 +0000

    add lists with tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,8 @@
-const Task = (task) => {
+const Task = ({ task }) => {
   return (
     <div>
       {" "}
-      <h3> {`${task.text}`} </h3>
+      <h3> {`${task.text} ${task.target_reps} / ${task.completed_reps} `}</h3>
     </div>
   );
 };
