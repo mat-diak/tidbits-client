e18bd40e47e390fcd1a4d9dd48b7fb019526790c
mikitaro-27 <matt.diak@gmail.com>
2022-03-06 18:36:04 +0000

    makes posts accessible with token

--- src/App.js
+++ src/App.js
@@ -1,59 +1,13 @@
 import Dashboard from "./pages/Dashboard";
 import Hello from "./pages/Hello";
 import Header from "./components/Header";
-import axiosRestApi from "./components/axios";
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Container from "react-bootstrap/Container";
 
+
 const App = () => {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    async function fetchTasks() {
-      const req = await axiosRestApi.get("/api/tasks");
-      setTasks(req.data);
-    }
-
-    fetchTasks();
-  }, []);
-
-  // Add Count to Completed Reps
-  const onDone = async (id) => {
-    const consideredTask = tasks
-      .filter((task) => {
-        return task._id === id;
-      })
-      .at(0);
-
-    if (consideredTask.completedReps < consideredTask.targetReps) {
-      const req = await axiosRestApi.put(`/api/tasks/${id}`, {
-        $inc: {
-          completedReps: 1,
-        },
-      });
-      const updatedTask = req.data;
-      setTasks(
-        tasks.map((task) => (task._id === updatedTask._id ? updatedTask : task))
-      );
-    } else {
-      alert("This is completed");
-    }
-  };
-
-  const addTask = async (text, reps) => {
-    const task = {
-      text: text,
-      completedReps: 0,
-      targetReps: reps,
-    };
-
-    const req = await axiosRestApi.post("api/tasks", task);
-
-    setTasks([...tasks, req.data]);
-  };
 
   return (
     <>
@@ -64,7 +18,7 @@ const App = () => {
             <Route
               path="/"
               element={
-                <Dashboard tasks={tasks} onAdd={addTask} onDone={onDone} />
+                <Dashboard />
               }
             />
             <Route path="/hello" element={<Hello />} />
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,27 @@
 import AddTask from "../components/AddTask";
 import TaskList from "../components/TaskList";
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import axiosRestApi from "../components/axios";
 
-function Dashboard({ tasks, onAdd, onDone }) {
+function Dashboard() {
   const navigate = useNavigate()
   const { user } = useSelector((state) => state.auth)
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      const req = await axiosRestApi.get("/api/tasks", {
+        headers: {
+          Authorization: `Bearer ${user.token}`
+        }
+      });
+      setTasks(req.data);
+    }
+
+    fetchTasks();
+  }, []);
 
   // redirects to Hello page if not logged in
   useEffect(() => {
@@ -15,15 +30,66 @@ function Dashboard({ tasks, onAdd, onDone }) {
     }
   }, [user, navigate])
 
+  // Add Count to Completed Reps
+  const onDone = async (id) => {
+    const consideredTask = tasks
+      .filter((task) => {
+        return task._id === id;
+      })
+      .at(0);
+
+    if (consideredTask.completedReps < consideredTask.targetReps) {
+      
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`
+        }
+      }
+
+      const req = await axiosRestApi.put(`/api/tasks/${id}`, {
+        $inc: {
+          completedReps: 1,
+        },
+      }, config);
+
+      const updatedTask = req.data;
+      setTasks(
+        tasks.map((task) => (task._id === updatedTask._id ? updatedTask : task))
+      );
+    } else {
+      alert("This is completed");
+    }
+  };
+
+  const addTask = async (text, reps) => {
+    const task = {
+      text: text,
+      completedReps: 0,
+      targetReps: reps,
+      user: user.id
+    };
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    }
+
+    const req = await axiosRestApi.post("/api/tasks", task, config);
+
+    setTasks([...tasks, req.data]);
+  };
+
+
 
   return (
     <div>
       <h1>Snacks</h1>
-      <AddTask onAdd={onAdd} />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <TaskList tasks={tasks} onDone={onDone} />
       ) : (
-        "There is currently no tasks"
+        "You have no tasks"
       )}
     </div>
   );
