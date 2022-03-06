6b05db0379d34b669e3fd41bb02a3db616623088
mikitaro-27 <matt.diak@gmail.com>
2022-03-06 10:42:48 +0000

    Creates authService and authSlice methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { Provider } from 'react-redux'
-import {store } from './app/store'
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -9,7 +9,7 @@ import App from "./App";
 
 ReactDom.render(
   <Provider store={store}>
-    <App /> 
+    <App />
   </Provider>,
   document.getElementById("root")
 );
