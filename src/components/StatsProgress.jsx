7902fb3dd2a01a75e2fa6e7c396cd6df0c5ecfb5
Jason Humbert <jasonhumbert@outlook.com>
2022-03-08 01:44:17 +0000

    Modify StatsModal format

--- src/components/StatsProgress.jsx
+++ src/components/StatsProgress.jsx
@@ -6,19 +6,19 @@ const StatsProgress = ({ tasks }) => {
     <div className="progress-section">
       {tasks && tasks.map((task) => (
         <div key={task._id} className="progress-task-card">
-          {/* <div className="progress-task-text">{task.text}</div> */}
-          {/* <div className="progress-bar-task"> */}
+          <div className="progress-bar-task">
+            <div className="progress-task-text">{task.text}</div>
             <div className="progress-task-reps">{task.completedReps}/{task.targetReps}</div>
-            <ProgressLine
-              label={task.text}
-              visualParts={[
-                {
-                  percentage: `${(task.completedReps / task.targetReps) * 100}%`,
-                  color: "green"
-                }
-              ]}
-            />
-          {/* </div> */}
+          </div>
+          <ProgressLine
+            label={null}
+            visualParts={[
+              {
+                percentage: `${(task.completedReps / task.targetReps) * 100}%`,
+                color: "lightgreen"
+              }
+            ]}
+          />
         </div>
       ))}
     </div>
