be3e172f4d8acc0fa454d5b4e311124c5f5105c6
naima <naimaosman203@gmail.com>
2022-03-10 14:26:47 +0000

    styling for registration page

--- src/pages/Hello.jsx
+++ src/pages/Hello.jsx
@@ -10,6 +10,8 @@ import Spinner from "../components/Spinner";
 import "./Hello.css";
 
 function Hello() {
+  // const [switcher, setSwitcher] = useState(false);
+
   const [registerData, setRegisterData] = useState({
     registerName: "",
     registerEmail: "",
@@ -90,11 +92,76 @@ function Hello() {
     return <Spinner />;
   }
 
+  // const [switcher, setSwitcher] = useState(false)
+
+  // const handleSwitch = () => {
+  //   setSwitcher(s => !s)
+  // }
+
+  const handleSwitch = () => {
+    const switchers = [...document.querySelectorAll('.switcher')]
+    switchers.forEach(item => {
+	    item.addEventListener('click', function() {
+		    switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		    this.parentElement.classList.add('is-active')
+	   })
+    })
+  }
+
   return (
     <>
-      <Container className="login-container">
-        <div className="row d-flex justify-content-around">
-          <div className="col-3">
+  <section className="forms-section">
+  <h1 className="section-title">Welcome</h1>
+  <div className="forms">
+    <div className="form-wrapper is-active">
+      <button type="button" className="switcher switcher-login" onClick={handleSwitch}>
+        Login
+        <span className="underline"></span>
+      </button>
+      <form className="form form-login">
+        <fieldset>
+          <legend>Please, enter your email and password for login.</legend>
+          <div className="input-block">
+            <label htmlFor="login-email">E-mail</label>
+            <input id="login-email" type="email" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="login-password">Password</label>
+            <input id="login-password" type="password" required />
+          </div>
+        </fieldset>
+        <button type="submit" className="btn-login">Login</button>
+      </form>
+    </div>
+    <div className="form-wrapper">
+      <button type="button" className="switcher switcher-signup" onClick={handleSwitch}>
+        Sign Up
+        <span className="underline"></span>
+      </button>
+      <form className="form form-signup">
+        <fieldset>
+          <legend>Please, enter your email, password and password confirmation for sign up.</legend>
+          <div className="input-block">
+            <label htmlFor="signup-email">E-mail</label>
+            <input id="signup-email" type="email" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="signup-password">Password</label>
+            <input id="signup-password" type="password" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="signup-password-confirm">Confirm password</label>
+            <input id="signup-password-confirm" type="password" required />
+          </div>
+        </fieldset>
+        <button type="submit" className="btn-signup">Continue</button>
+      </form>
+    </div>
+  </div>
+</section>
+      {/* <Container classNameName="login-container">
+        <div classNameName="row d-flex justify-content-around">
+          <div classNameName="col-3">
             <Login
               onSubmit={onLoginSubmit}
               onChange={onLoginChange}
@@ -102,7 +169,7 @@ function Hello() {
             />
           </div>
 
-          <div className="col-3">
+          <div classNameName="col-3">
             <Register
               onSubmit={onRegisterSubmit}
               onChange={onRegisterChange}
@@ -110,7 +177,7 @@ function Hello() {
             />
           </div>
         </div>
-      </Container>
+      </Container> */}
     </>
   );
 }
