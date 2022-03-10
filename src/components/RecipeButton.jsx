288ac99229d686117dd8ecbe8030767b995ceeb9
mikitaro-27 <matt.diak@gmail.com>
2022-03-10 11:25:36 +0000

    Changes Add to my tidbits to Add

--- src/components/RecipeButton.jsx
+++ src/components/RecipeButton.jsx
@@ -1,7 +1,7 @@
 const RecipeButton = ({ task, onRecipe }) => {
   return (
     <button className="btn btn-outline-info" onClick={() => onRecipe(task)}>
-      Add to my tidbits
+      Add
     </button>
   );
 };
