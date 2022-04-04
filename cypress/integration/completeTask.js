55170080e4b4f863c437098abccce28bd8ca4225
mikitaro-27 <matt.diak@gmail.com>
2022-04-04 18:19:58 +0100

    Refactors test

--- cypress/integration/completeTask.js
+++ cypress/integration/completeTask.js
@@ -1,49 +1,27 @@
 describe("user completes a task", () => {
   it("task moves to completed", () => {
     cy.login();
-    cy.addTask("Task to complete");
+    cy.addTask("Go for a run");
 
-    const res1 = {
-      _id: "624ad0ce788effc434932e66",
-      user: "6242d3f25729960c54a1d255",
-      text: "Go for a run",
-      targetReps: 3,
-      completedReps: 1,
-      endDate: "2022-04-08T16:40:27.425Z",
-      createdAt: "2022-04-04T16:40:27.427Z",
-      updatedAt: "2022-04-04T16:40:27.427Z",
-      __v: 0,
-    };
-
-    const res2 = {
-      _id: "624ad0ce788effc434932e66",
-      user: "6242d3f25729960c54a1d255",
-      text: "Go for a run",
-      targetReps: 3,
-      completedReps: 2,
-      endDate: "2022-04-08T16:40:27.425Z",
-      createdAt: "2022-04-04T16:40:27.427Z",
-      updatedAt: "2022-04-04T16:40:27.427Z",
-      __v: 0,
-    };
-
-    const res3 = {
-      _id: "624ad0ce788effc434932e66",
-      user: "6242d3f25729960c54a1d255",
-      text: "Go for a run",
-      targetReps: 3,
-      completedReps: 3,
-      endDate: "2022-04-08T16:40:27.425Z",
-      createdAt: "2022-04-04T16:40:27.427Z",
-      updatedAt: "2022-04-04T16:40:27.427Z",
-      __v: 0,
-    };
+    const res = (completedReps) => {
+      return {
+        _id: "624ad0ce788effc434932e66",
+        user: "6242d3f25729960c54a1d255",
+        text: "Go for a run",
+        targetReps: 3,
+        completedReps: completedReps,
+        endDate: "2022-04-08T16:40:27.425Z",
+        createdAt: "2022-04-04T16:40:27.427Z",
+        updatedAt: "2022-04-04T16:40:27.427Z",
+        __v: 0,
+      }
+    }
 
     // stub the put request
     cy.intercept(
       "PUT",
       "http://localhost:5000/api/tasks/624ad0ce788effc434932e66",
-      res1
+      res(1)
     );
 
     cy.get("button.done-btn").click();
@@ -51,7 +29,7 @@ describe("user completes a task", () => {
     cy.intercept(
       "PUT",
       "http://localhost:5000/api/tasks/624ad0ce788effc434932e66",
-      res2
+      res(2)
     );
 
     cy.get("button.done-btn").click();
@@ -59,7 +37,7 @@ describe("user completes a task", () => {
     cy.intercept(
       "PUT",
       "http://localhost:5000/api/tasks/624ad0ce788effc434932e66",
-      res3
+      res(3)
     );
 
     cy.get("button.done-btn").click();
